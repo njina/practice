29fa91eb6c5b2ff66057b56135be6d0917734f6b
njina <najina603@gmail.com>
2020-07-01 14:02:24 +0900

    애플 예제

--- c_example/apple/js/src/apple.js
+++ c_example/apple/js/src/apple.js
@@ -3,13 +3,14 @@
 
 
 (function($){
-	// jQuery 시작 
-//공통 변수
+
 	var win          = $(window);
 	var winH         = win.innerHeight();
-	var winHPart     = winH / 3 * 2;
+	var winHPart     = winH / 4 * 3;
+	
+	win.scrollTop(0);
 
-// #openLaptop 변수
+// #openLaptop
 	var openLaptop   = $('#openLaptop');
 	var openH2       = openLaptop.find('h2');
 	var macbook      = openLaptop.find('.macbook');
@@ -18,26 +19,28 @@
 	var j='0000';
 	var imgLength = 92;
 
-// #laptopSize 변수
-	var laptopSize  = $('#laptopSize');
-	var laptopSizeOffset = laptopSize.offset().top;
+// #laptopSize
+	var laptopSize        = $('#laptopSize');
+	var laptopSizeOffset  = laptopSize.offset().top;
 	var laptopSizeMacbook = laptopSize.find('.macbook');
-	var macbookDiv = laptopSizeMacbook.children('div').children('div');
-	var macbookDl = macbookDiv.find('dl');
-	var macbookVideo = laptopSize.find('.macbook_video');
+	var macbookDiv        = laptopSizeMacbook.children('div').children('div');
+	var macbookDl         = macbookDiv.children('dl');
+	var macbookVideo      = laptopSize.find('.macbook_video');
 
-//retina display 변수
-	var retinaDisplay = $('#retinaDisplay');
-	var retinaImg = retinaDisplay.find('.retina_image');
-	var people = retinaImg.find('.people');
-	var artwork = retinaImg.find('.artwork');
+// #retinaDisplay
+	var retinaDisplay     = $('#retinaDisplay');
+	var retinaImg         = $('.retina_image');
+	var people            = retinaImg.find('.people');
+	var artwork           = retinaImg.find('.artwork');
+	var artworkDp           = retinaImg.find('.artwork_display');
+
+	
 
 
-
-
+// 기능 수행============================================================================
 	// #openLaptop영역 처리  ---------------------------------------------------
 
-	openH2.animate({opacity:1, top:0}, 800);
+	openH2.animate({opacity:1, top:0, lineHeight:6+'rem'}, 800);
 	openLaptop.find('p').delay(200).animate({opacity:1, top:0}, 800);
 
 	for(var i=0; i<imgLength; i++){
@@ -74,7 +77,7 @@ var secondScrollStart = 850;
 			imgSelect = imgLength-1;			
 		}
 
-		console.log(secondScrollStart);
+		// console.log(secondScrollStart);
 
 		macbook.find('img').eq( imgSelect ).show();
 		macbook.find('img').eq( imgSelect ).siblings().hide();
@@ -88,30 +91,39 @@ var secondScrollStart = 850;
 	// #openLaptop영역 처리 끝 ---------------------------------------------------
 
 	// #laptopSize 영역 처리 ------------------------------------------------
-	//두번째 영역 비디오영역에 .active 입력
-	// macbookVideo.addClass('active');
-	macbookDl.css({opacity:0});
 
-	//dl의 offset을 각각 파악
-	var dlOffset = [];
-	for(var i = 0; i < macbookDl.length ; i++){
+	// $.each(macbookDl, function(data){	$(this).css({opacity:0});	});
+	macbookDl.css({opacity:0});
+	
+	// dl의 offset값을 각각 파악
+	var dlOffset = []; 
+	for( var i=0; i < macbookDl.length; i++ ){
 		dlOffset[i] = macbookDl.eq(i).offset().top;
 	}
-	console.log(dlOffset);
+	// console.log(dlOffset);
 
 	win.on('scroll', function(){
 		var winScroll = $(this).scrollTop();
 		var winScrollPlus = winScroll + winHPart;
-		var op;
+		var op = 0;
+		// laptopSize 위치값파악하여 동작체크
+		// if(winScrollPlus >= laptopSizeOffset ){
+		// 	// console.log('laptopSizeOffset 스타트!!!');
+		// 	var op1 =	(winScrollPlus - laptopSizeOffset) / 400;
+		// 	// console.log( op1 );
+		// 	laptopSize.css({opacity: op1});
+		// }
 
-		if(winScrollPlus >= laptopSizeOffset){
-			for(var i = 0; i< macbookDl.length; i++){
-				if( winScrollPlus >= dlOffset[i] ){
+		// laptopSize 위치값파악 후 dl값의 위치에따라 투명도 처리
+		if(winScrollPlus >= laptopSizeOffset ){
+			// dl값을 각각 파악하여 매번 순환체크하도록 처리
+			for(var i=0; i< macbookDl.length; i++){
+				if( winScrollPlus  >= dlOffset[i]){
 					op = (winScrollPlus - dlOffset[i]) / 400;
-					macbookDl.eq(i).css({opacity:op});
+					macbookDl.eq(i).css({opacity: op});
 				}
 			}
-			//동영상 동작하게
+			// 동영상 동작하게 만들기
 			if( !macbookVideo.hasClass('active') ){
 				macbookVideo.addClass('active');
 				macbookVideo.find('video').get(0).play();
@@ -119,54 +131,53 @@ var secondScrollStart = 850;
 		}
 	});
 
-//retina display
-var retinaImgOffset = retinaImg.offset().top;
-var retinaImgWidth = retinaImg.outerWidth();
-var winWidth = win.outerWidth();
-var retinaImgPercent = retinaImgWidth / winWidth * 100;
-// console.log(retinaImgPercent);
-retinaImg.css({width: retinaImgPercent + 'vw'});
-var rep = 0;
-var rep2 = 100;
-var rep3 = 0;
-var retinaLaptopGo = false;
-retinaImg.css({height:winH + 'px'});
-win.on('scroll',function(){
-	var winScroll = $(this).scrollTop();
-	var winScrollPlus = winScroll + winHPart;
+	// #retinaDisplay -------------------------------------------------------------
+	retinaImg.height(winH);
+	
+	var retinaImgOffset = retinaImg.offset().top;
+	var retinaImgWidth = retinaImg.outerWidth();
+	var winWidth       = win.outerWidth();
+	var retinaImgPercent = retinaImgWidth / winWidth * 100;
+	var retinaDptoImgOff = retinaDisplay.offset().top - retinaImgOffset;
 
-	if( winScrollPlus >= retinaImgOffset && rep <= 30){
-		rep = (winScrollPlus - retinaImgOffset) / winH * 30;		
-		retinaImg.css({width:retinaImgPercent + rep + 'vw'});
-	}
-
-	if( winScroll >= retinaImgOffset ){
-		rep2 = 100 - (winScrollPlus - retinaImgOffset) / winH * 100;
-		// console.log(rep2);
-		retinaImg.css({position:'fixed',top:0});
-		people.css({height:rep2 + '%'});
-		if( rep2 <= 0 ){
-			retinaLaptopGo = true;
+	// console.log(retinaImgPercent);
+	retinaImg.css({width: retinaImgPercent + 'vw'});
+	var rep = 0;
+	var rep2 = 100;
+	var rep3 = 0;
+	
+	win.on('scroll', function(){
+		var winScroll = $(this).scrollTop();
+		var winScrollPlus = winScroll + winHPart;
+		
+		if(winScrollPlus >= retinaImgOffset){
+			rep = (winScrollPlus - retinaImgOffset) / winH * 30;
+			var peopleWp = retinaImgPercent + rep;
+			(peopleWp < 100) ?retinaImg.css({width:peopleWp + 'vw'}) : retinaImg.css({width:100 + 'vw'});
 		}
-	} else {
-		retinaImg.css({position:'relative', top:'auto'});
-	}
-	if( retinaLaptopGo === true ){
-		rep3 = winScrollPlus - retinaImgOffset / winH * 30;
-		// retinaImg.css({width:, height:});
-	}
-});
 
+		if(winScroll >= retinaImgOffset){
 
+			rep2 = 120 - (winScroll - retinaImgOffset) / winH * 100;
+			retinaDisplay.css({position:'fixed', top:retinaDptoImgOff});
+			people.css({height:rep2 + '%'});
+			artworkDp.css({filter:'grayscale('+ rep2*0.1 +')'});	
 
+		}else{retinaDisplay.css({position:'relative', top:0});	}
 
+		// if(rep2 <= 0){
+		// 	rep3 = winScroll - retinaImgOffset + winH / winH * 30;
+		// 	console.log( rep3 )
+		// 	// retinaImg.css({width:, height:});
+		// }
 
-
-
-
-
-
-
-	
-	
-	})(jQuery);
+		if(winScroll >= retinaImgOffset + winH){
+			rep3 = 2 - ((winScroll - (retinaImgOffset + winH)) / winH);
+			console.log((winScroll - (retinaImgOffset + winH)));
+			
+			if(rep3 >= 0.7){artwork.css({transform:'scale('+rep3+')'});}
+			if(rep3 <= 0) {retinaDisplay.css({top: retinaDptoImgOff + (rep3*winH*2)+'px'})}
+		}
+	});
+	// ----------------------------------------------------------------------
+})(jQuery);
